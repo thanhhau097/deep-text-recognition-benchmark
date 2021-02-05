ff650400f93a43c8843bac9608280e2e535e7a36
Lionel <thanhhau097@gmail.com>
2021-02-05 09:54:38 +0000

    refactor core.py

--- lionelocr/core.py
+++ lionelocr/core.py
@@ -16,42 +16,14 @@ from lionelocr.model import Model
 
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
 parser = argparse.ArgumentParser()
-# parser.add_argument('--workers', type=int, help='number of data loading workers', default=4)
-# parser.add_argument('--batch_size', type=int, default=192, help='input batch size')
-# parser.add_argument('--saved_model', help="path to saved_model to evaluation")
-# """ Data processing """
-# parser.add_argument('--batch_max_length', type=int, default=25, help='maximum-label-length')
-# parser.add_argument('--imgH', type=int, default=32, help='the height of the input image')
-# parser.add_argument('--imgW', type=int, default=100, help='the width of the input image')
-# parser.add_argument('--rgb', action='store_true', help='use rgb input')
-# parser.add_argument('--character', type=str, default='0123456789abcdefghijklmnopqrstuvwxyz', help='character label')
-# parser.add_argument('--sensitive', action='store_true', help='for sensitive character mode')
-# parser.add_argument('--PAD', action='store_true', help='whether to keep ratio then pad for image resize')
-# """ Model Architecture """
-# parser.add_argument('--Transformation', type=str, help='Transformation stage. None|TPS', default='TPS')
-# parser.add_argument('--FeatureExtraction', type=str, help='FeatureExtraction stage. VGG|RCNN|ResNet', default='ResNet')
-# parser.add_argument('--SequenceModeling', type=str, help='SequenceModeling stage. None|BiLSTM', default='BiLSTM')
-# parser.add_argument('--Prediction', type=str, help='Prediction stage. CTC|Attn', default='Attn')
-# parser.add_argument('--num_fiducial', type=int, default=20, help='number of fiducial points of TPS-STN')
-# parser.add_argument('--input_channel', type=int, default=1, help='the number of input channel of Feature extractor')
-# parser.add_argument('--output_channel', type=int, default=512,
-#                     help='the number of output channel of Feature extractor')
-# parser.add_argument('--hidden_size', type=int, default=256, help='the size of the LSTM hidden state')
-
-# opt = parser.parse_args()
 opt = parser.parse_known_args()[0]
-# """ vocab / character number configuration """
-# if opt.sensitive:
-#     opt.character = string.printable[:-6]  # same with ASTER setting (use 94 char).
 
 cudnn.benchmark = True
 cudnn.deterministic = True
 opt.num_gpu = torch.cuda.device_count()
 
 
-# TODO: save all config to weights
 class OCRModel():
     def __init__(self, weights_path):
         self.opt = opt
